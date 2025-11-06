2ca5937a37baa7a1783849c816f6bb1f897158ed
Daniel Adeleke <danieladeleke05@gmail.com>
2025-11-06 16:53:26 +0100

    Made the background of the app an image

--- lib/generate_random_quote.dart
+++ lib/generate_random_quote.dart
@@ -5,6 +5,28 @@ class GenerateRandomQuote extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: BackgroundImage(),
+    );
+  }
+}
+
+class BackgroundImage extends StatelessWidget {
+  const BackgroundImage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.maxFinite,
+      width: double.maxFinite,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/actual quote app background.jpeg'),
+          fit: BoxFit.cover
+        )
+      ),
+    );
   }
 }
