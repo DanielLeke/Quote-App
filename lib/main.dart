17453e196e63a98634b23302b8100add76b5e8a0
Daniel Adeleke <danieladeleke05@gmail.com>
2025-11-06 16:42:54 +0100

    Created a stateless widget for quote generation and linked it to the main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:quote_app/generate_random_quote.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorSchemeSeed: Color(0xFF269AA2)
       ),
-      home: Scaffold(),
+      home: GenerateRandomQuote(),
     );
   }
 }
