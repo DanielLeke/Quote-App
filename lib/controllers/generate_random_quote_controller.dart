d101cde5ce0132e18d67453dc10db16684dd5a63
Daniel Adeleke <danieladeleke05@gmail.com>
2025-11-06 21:57:49 +0100

    Added a get request to get the quote info from the server

--- lib/controllers/generate_random_quote_controller.dart
+++ lib/controllers/generate_random_quote_controller.dart
@@ -1,4 +1,5 @@
 import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
 
 class GenerateRandomQuoteController extends GetxController {
   final _quote = "".obs;
@@ -7,7 +8,10 @@ class GenerateRandomQuoteController extends GetxController {
   String get quote => _quote.value;
   String get author => _author.value;
 
-  void getRandomQuote() async {
-    final serverUrl = Uri.parse("https://quote-app-backend-d8ga.onrender.com/randomquote");
+  Future<void> getRandomQuote() async {
+    final serverUrl = Uri.parse(
+      "https://quote-app-backend-d8ga.onrender.com/randomquote",
+    );
+    final quoteInfo = await http.get(serverUrl);
   }
 }
