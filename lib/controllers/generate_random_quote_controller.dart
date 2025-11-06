12aee400850a87852190cf9c8124564b19f49154
Daniel Adeleke <danieladeleke05@gmail.com>
2025-11-06 21:44:18 +0100

    Created the variables that will help the functionality come to life

--- lib/controllers/generate_random_quote_controller.dart
+++ lib/controllers/generate_random_quote_controller.dart
@@ -1,3 +1,9 @@
 import 'package:get/get.dart';
 
-class GenerateRandomQuoteController extends GetxController{}
+class GenerateRandomQuoteController extends GetxController {
+  final _quote = "".obs;
+  final _author = "".obs;
+
+  String get quote => _quote.value;
+  String get author => _author.value;
+}
