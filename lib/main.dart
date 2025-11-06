ff415e2bcef45207d7e39cfe1136a81f2262dec3
Daniel Adeleke <danieladeleke05@gmail.com>
2025-11-06 16:32:42 +0100

    Inserted a scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorSchemeSeed: Color(0xFF269AA2)
+      ),
+      home: Scaffold(),
+    );
   }
 }
